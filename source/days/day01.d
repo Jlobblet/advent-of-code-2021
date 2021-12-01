29cad7092538ad7b8db0c9570ac6e7b71e669f0f
Jlobblet <jlobblet-github@gmx.co.uk>
2021-12-01 16:54:45 +0000

    Abstract some more stuff out

--- source/days/day01.d
+++ source/days/day01.d
@@ -4,26 +4,30 @@ import std.algorithm : count, map, sum;
 import std.array : array;
 import std.conv : to;
 import std.functional : compose;
-import std.range : dropBackOne, dropOne, slide, zip;
+import std.range : dropBackOne, dropOne, slide, zip, StoppingPolicy;
 import std.string : splitLines, strip;
 import days.day : Day;
 import timer : Timer;
 
-class Day01 : Day!(ulong[], size_t, "data/01.txt")
+class Day01 : Day!(ulong[], size_t, "bigdata/01.txt")
 {
     ulong[] parseData(string data, Timer* timer)
     {
         return data.splitLines.map!(compose!(to!ulong, strip)).array;
     }
 
+    size_t problem(size_t offset)(ulong[] data)
+    {
+        return StoppingPolicy.shortest.zip(data, data[offset..$]).count!"a[1] > a[0]";
+    }
+
     size_t problemA(ulong[] data, Timer* timer)
     {
-        return zip(data.dropBackOne, data.dropOne).count!"a[1] > a[0]";
+        return problem!1(data);
     }
 
     size_t problemB(ulong[] data, Timer* timer)
     {
-        auto windows = data.slide(3);
-        return zip(windows.dropBackOne, windows.dropOne).count!"a[1].sum > a[0].sum";
+        return problem!3(data);
     }
 }
